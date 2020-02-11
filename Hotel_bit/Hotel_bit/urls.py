f4991a6e2ab6bb4c2d5f71dbad50744a22a02cbf
matium20 <60585657+matium20@users.noreply.github.com>
2020-02-10 22:44:14 -0300

    agrege static a todos los html

--- Hotel_bit/Hotel_bit/urls.py
+++ Hotel_bit/Hotel_bit/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-
+from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.home), #añadi las urls que tendrá el sistema de reservas en un path solo ''
+    path('home', views.home), #añadi las urls que tendrá el sistema de reservas en un path solo 'home/'
     path('reservar/', include('reservas.urls')), #añadi las urls que tendrá el sistema de reservas en un path solo 'reservas/'
     path('contacto/', include('contacto.urls')), #añadi las urls que tendrá el sistema de reservas en un path solo 'contacto/'
 ]
