632042efff43c7a7a608aa8d7030cc985f846590
matium20 <60585657+matium20@users.noreply.github.com>
2020-02-12 13:08:30 -0300

    Agregue template base, agregue galeria y login (para editar), cambios en algunas url

--- Hotel_bit/Hotel_bit/urls.py
+++ Hotel_bit/Hotel_bit/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('galeria', views.galeria), #añadi las url galeria
     path('sobre_nosotros', views.sobre_nosotros), #añadi las url sobre_nosotros
     path('habitacion_vip', views.habitacion_vip), #añadi las url habitacion_vip
+    path('login', views.login), #añadi las url login
     path('reservar/', include('reservas.urls')), #añadi las urls que tendrá el sistema de reservas en un path solo 'reservas/'
     path('contacto/', include('contacto.urls')), #añadi las urls que tendrá el sistema de reservas en un path solo 'contacto/'
 ]
--- Hotel_bit/Hotel_bit/views.py
+++ Hotel_bit/Hotel_bit/views.py
@@ -9,10 +9,14 @@ def habitaciones(request):
     return render(request, 'room.html', {})
 
 def galeria(request):
-    return render(request, 'index.html', {}) 
+    return render(request, 'galeria.html', {}) 
 
 def sobre_nosotros(request):
     return render(request, 'about.html', {})
 
 def habitacion_vip(request):
     return render(request, 'single-room.html', {})
+
+def login(request):
+    return render(request, 'login.html', {})
+
