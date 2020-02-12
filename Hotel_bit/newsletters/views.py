56fc49767ee7c0bd0e8ea1ec5aecb087cee4e487
matium20 <60585657+matium20@users.noreply.github.com>
2020-02-12 13:19:53 -0300

    cambios en newsletter

--- Hotel_bit/newsletters/views.py
+++ Hotel_bit/newsletters/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
 # Create your views here.
+from django.shortcuts import render
+from .forms import NewsletterForm
+# Create your views here.
+
+def contacto(request):
+    template = "base.html"
+
+    if request.method == "POST":
+        form = NewsletterForm.POST)
+
+        if form.is_valid():
+            form.save()
+    
+    else:
+        form = NewsletterForm
+
+    context = {
+    'form': form,
+    }
+    return render (request, template, context)
