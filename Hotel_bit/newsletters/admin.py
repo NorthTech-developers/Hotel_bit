002e52b4a4088c41a5dc86321d940844b2ce3e63
matium20 <60585657+matium20@users.noreply.github.com>
2020-02-12 23:19:00 -0300

    cambios generales

--- Hotel_bit/newsletters/admin.py
+++ Hotel_bit/newsletters/admin.py
@@ -1,8 +1,6 @@
 from django.contrib import admin
 from .models import NewsletterUser
+
 # Register your models here.
 
-class NewsletterAdmin(admin.ModelAdmin):
-    list_display = ('email_newsletter', 'date_added',)
-
-admin.site.register(NewsletterUser, NewsletterAdmin)
+admin.site.register(NewsletterUser)
