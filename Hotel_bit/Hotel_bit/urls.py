8e165eb95f5ff3ca1b05a7c0a6f1870981d82bca
matium20 <60585657+matium20@users.noreply.github.com>
2020-02-17 21:25:13 -0300

    cambios en Newsletter, ya funcionando

--- Hotel_bit/Hotel_bit/urls.py
+++ Hotel_bit/Hotel_bit/urls.py
@@ -16,13 +16,15 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+
 urlpatterns = [
 
+    
     # login y registro
     # path('', views.welcome),
     # path('register', views.register),
     # path('login', views.login),
-    path('logout', views.logout),
+    # path('logout', views.logout),
     
     # cambie los Path de login y registro por el include de abajo 
     path('', views.home), #añadi las url home de inicio
@@ -35,8 +37,9 @@ urlpatterns = [
     path('sobre_nosotros/', views.sobre_nosotros), #añadi las url sobre_nosotros
     path('habitacion_vip/', views.habitacion_vip), #añadi las url habitacion_vip
     path('login/', views.login), #añadi las url login
-    path('registro/', views.registro), #añadi las url login
-    path('reservar/', include('reservas.urls')), #añadi las urls que tendrá el sistema de reservas en un path solo 'reservas/'
+    path('usuario/', include('users.urls')), #añadi las urls que tendrá el sistema de reservas en un path solo 'reservas/'
+    path('reservar/', include('reservas.urls')), #añadi las urls que tendrá el sistema de reservas en un path solo 'users/'
     path('contacto/', include('contacto.urls')), #añadi las urls que tendrá el sistema de reservas en un path solo 'contacto/'
-    path('usuario/', include('users.urls')),
+   
+    
 ]
--- Hotel_bit/contacto/forms.py
+++ Hotel_bit/contacto/forms.py
@@ -4,4 +4,4 @@ from .models import contacto
 class ContactoForm(forms.ModelForm):
     class Meta:
         model = contacto
-        fields = ('first_name', 'last_name', 'phone', 'email', 'message')
+        fields = ('first_name',  'last_name',  'phone',  'email',  'message')
