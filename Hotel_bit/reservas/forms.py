1e77c4152f08a68032b95ee92cfedbc376346eec
Daniel Ferreira <51835115+Daniferpro@users.noreply.github.com>
2020-02-29 19:09:26 -0300

    agregue algunos modelos para mirar

--- Hotel_bit/reservas/forms.py
+++ Hotel_bit/reservas/forms.py
@@ -1,5 +1,43 @@
 from django import forms
 from .models import Habitacion, Reserva_habitacion
+from .models import Hotel, Habitaciones, Reserva, Tipo_alojamiento, Tipo_pension, Precio_pension, Reservas_habitacion
+
+class ReservaForm(forms.ModelForm):
+	class Meta:
+		model = Reserva
+		fields = '__all__'
+		widgets = {
+            'fecha_reserva': forms.widgets.SelectDateWidget(years=range(2019, 2021))
+        }
+
+class TipoAlojamientoForm(forms.ModelForm):
+	class Meta:
+		model = Tipo_alojamiento
+		fields = '__all__'
+
+class TipoPensionForm(forms.ModelForm):
+	class Meta:
+		model = Tipo_pension
+		fields = '__all__'
+
+class PrecioPensionForm(forms.ModelForm):
+	class Meta:
+		model = Precio_pension
+		fields = '__all__'
+
+class ReservasHabitacionForm(forms.ModelForm):
+	class Meta:
+		model = Reservas_habitacion
+		fields = 'id', 'fecha_entrada', 'fecha_salida', 'ocupantes', 'reserva_habitacion'
+		widgets = {
+            'fecha_entrada': forms.widgets.SelectDateWidget(years=range(2019, 2021)),
+            'fecha_salida': forms.widgets.SelectDateWidget(years=range(2019, 2021)),
+        }
+		fecha_entrada = 'fecha_entrada'
+		fecha_salida = 'fecha_salida'
+		if fecha_entrada > fecha_salida:
+			raise forms.ValidationError("Fecha de salida debe ser mayor a la fecha de entrada")
+
 
 class ReservaHabitacion(forms.ModelForm):
 
--- Hotel_bit/reservas/models.py
+++ Hotel_bit/reservas/models.py
@@ -1,4 +1,136 @@
 from django.db import models
+from django.contrib.auth.models import User
+import datetime
+
+# Create your models here.
+
+precio_pension = (
+	(150.00, 150.00),
+	(100.00, 100.00),
+	(60.00, 60.00),
+	(0.00, 0.00)
+)
+
+pension = (
+	('completa', 'completa'),
+	('media', 'media'),
+	('desayuno', 'desayuno'),
+	('nada', 'nada')
+)
+
+tipo_alojamiento = (
+	('doble', 'doble'),
+	('individual', 'individual')
+)
+
+precio_alojamiento = (
+	(200, 200),
+	(100, 100)
+)
+
+class Hotel(models.Model):
+	nombre = models.CharField(max_length=50)
+	ubicacion = models.CharField(max_length=50)
+	ciudad = models.CharField(max_length=50)
+	estrellas = models.IntegerField(default=0)
+	image = models.ImageField(upload_to='hotel/')
+
+	def __str__(self):
+		return self.nombre
+
+	class Meta:
+		verbose_name = 'Hotel'
+		verbose_name_plural = 'Hoteles'
+
+class Tipo_pension(models.Model):
+	tipo_pension = models.CharField(choices=pension, max_length=50)
+
+	def __str__(self):
+		return self.tipo_pension
+
+	class Meta:
+		verbose_name = 'Tipo Pension'
+		verbose_name_plural = 'Tipo Pensiones'
+
+class Precio_pension(models.Model):
+	precio = models.DecimalField(choices=precio_pension, max_digits=5, decimal_places=2)
+	precio_tipo_pension = models.ForeignKey('Tipo_pension', on_delete=models.CASCADE)
+	hotel_tipo_pension = models.ForeignKey('Hotel', on_delete=models.CASCADE)
+
+	def __str__(self):
+		return str(self.precio_tipo_pension)
+
+	class Meta:
+		verbose_name = 'Precio Pension'
+		verbose_name_plural = 'Precio Pensiones'
+
+class Habitaciones(models.Model):
+	descripcion = models.CharField(max_length=150)
+	image = models.ImageField(upload_to='habitaciones/')
+	num_habitacion = models.IntegerField(default=0)
+	baños = models.IntegerField(default=0)
+	garaje = models.IntegerField(default=0)
+	camas = models.IntegerField(default=0)
+	habitacion_hotel = models.ForeignKey('Hotel', on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.descripcion
+
+	class Meta:
+		verbose_name = 'Habitacion'
+		verbose_name_plural = 'Habitaciones'
+
+class Tipo_alojamiento(models.Model):
+	descripcion = models.CharField(choices=tipo_alojamiento, max_length=50)
+	precio = models.DecimalField(choices=precio_alojamiento, max_digits=5, decimal_places=2)
+	habitacion_tipo_alojamiento = models.ForeignKey('Habitaciones', on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.descripcion
+
+	class Meta:
+		verbose_name = 'Tipo Alojamiento'
+		verbose_name_plural = 'Tipos Alojamientos'
+
+class Reserva(models.Model):
+	reserva = models.ForeignKey(User, on_delete=models.CASCADE)
+	fecha_reserva = models.DateField(default=datetime.date.today)
+
+	def __str__(self):
+		return str(self.fecha_reserva)
+
+	class Meta:
+		verbose_name = 'Reserva'
+		verbose_name_plural = 'Reservas'
+
+class Valoraciones(models.Model):
+	fecha = models.DateField()
+	valoracion = models.IntegerField(default=0)
+	valoracion_usuario = models.ForeignKey(User, on_delete=models.CASCADE)
+	valoracion_reserva = models.ForeignKey('Reserva', on_delete=models.CASCADE)
+
+	def __str__(self):
+		return self.valoracion_usuario
+
+	class Meta:
+		verbose_name = 'Valoración'
+		verbose_name_plural = 'Valoraciones'
+
+class Reservas_habitacion(models.Model):
+	fecha_entrada = models.DateField()
+	fecha_salida = models.DateField()
+	ocupantes = models.IntegerField(default=0)
+	reserva_habitacion = models.ForeignKey('Habitaciones', on_delete=models.CASCADE)
+	reserva_reserva = models.ForeignKey('Reserva', on_delete=models.CASCADE)
+
+	def __str__(self):
+		return str(self.reserva_habitacion)
+
+	class Meta:
+		verbose_name = 'Reserva habitacion'
+		verbose_name_plural = 'Reservas habitaciones'
+
+# Los modelos de arriba los agregue hoy
 
 
 class Habitacion(models.Model):
